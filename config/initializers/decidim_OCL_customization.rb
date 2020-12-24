ac054e5ee7335c6b0fbf2b076cd972d797dea04e
Thomas Burkhalter <new.mysteryman@gmail.com>
2020-12-24 13:55:49 +0100

    Fix proposal problems with assemblies

--- config/initializers/decidim_OCL_customization.rb
+++ config/initializers/decidim_OCL_customization.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
 Decidim::DiffCell.include DecidimOCL::DiffCell
 Decidim::Proposals::ProposalMCell.include DecidimOCL::Proposals::ProposalMCell
 Decidim::ParticipatoryProcesses::ProcessMCell.include DecidimOCL::ParticipatoryProcesses::ProcessMCell
+Decidim::Proposals::Admin::ProposalNoteCreatedEvent.prepend DecidimOCL::Proposals::Admin::ProposalNoteCreatedEvent
