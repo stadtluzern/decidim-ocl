cbf0f87ba37d19daa75267779de70e3d4749768f
Carlo Beltrame <carlo@beltra.me>
2021-05-27 17:27:44 +0200

    Add more HTML blocks for styling the homepage

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -163,5 +163,35 @@ Decidim.configure do |config|
   # }
 end
 
+Decidim.content_blocks.register(:homepage, :html2) do |content_block|
+  content_block.cell = "decidim/content_blocks/html"
+  content_block.public_name_key = "decidim.content_blocks.html2.name"
+  content_block.settings_form_cell = "decidim/content_blocks/html_settings_form"
+
+  content_block.settings do |settings|
+    settings.attribute :html_content, type: :text, translated: true
+  end
+end
+
+Decidim.content_blocks.register(:homepage, :html3) do |content_block|
+  content_block.cell = "decidim/content_blocks/html"
+  content_block.public_name_key = "decidim.content_blocks.html3.name"
+  content_block.settings_form_cell = "decidim/content_blocks/html_settings_form"
+
+  content_block.settings do |settings|
+    settings.attribute :html_content, type: :text, translated: true
+  end
+end
+
+Decidim.content_blocks.register(:homepage, :html4) do |content_block|
+  content_block.cell = "decidim/content_blocks/html"
+  content_block.public_name_key = "decidim.content_blocks.html4.name"
+  content_block.settings_form_cell = "decidim/content_blocks/html_settings_form"
+
+  content_block.settings do |settings|
+    settings.attribute :html_content, type: :text, translated: true
+  end
+end
+
 Rails.application.config.i18n.available_locales = Decidim.available_locales
 Rails.application.config.i18n.default_locale = Decidim.default_locale
