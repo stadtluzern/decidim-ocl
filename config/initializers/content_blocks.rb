f2625ca43ce84f61d5b18f43df758f89679c30d8
Carlo Beltrame <carlo@beltra.me>
2021-06-25 17:07:50 +0200

    Add a content block for showing only the latest blog surveys. This doesn't work yet, because Decidim never adds surveys to the activity log for some reason

--- config/initializers/content_blocks.rb
+++ config/initializers/content_blocks.rb
@@ -34,3 +34,8 @@ Decidim.content_blocks.register(:homepage, :last_blog_entries) do |content_block
   content_block.cell = "decidim/content_blocks/last_blog_posts"
   content_block.public_name_key = "decidim.content_blocks.last_blog_posts.name"
 end
+
+Decidim.content_blocks.register(:homepage, :last_surveys) do |content_block|
+  content_block.cell = "decidim/content_blocks/last_surveys"
+  content_block.public_name_key = "decidim.content_blocks.last_surveys.name"
+end
