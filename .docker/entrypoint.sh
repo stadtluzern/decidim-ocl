0becad123880942854b1440d2f22dc5316058a10
Carlo Beltrame <carlo@beltra.me>
2021-08-11 15:52:37 +0200

    Use a simpler email for the initial account

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -6,11 +6,11 @@ bundle install
 
 bin/rails db:migrate RAILS_ENV=development
 
-echo "Decidim::System::Admin.where(email: 'decidim-ocl@puzzle.ch').first_or_initialize.update!(password: 'decidim', password_confirmation: 'decidim')" | bundle exec rails console
+echo "Decidim::System::Admin.where(email: 'decidim@puzzle.ch').first_or_initialize.update!(password: 'decidim', password_confirmation: 'decidim')" | bundle exec rails console
 
 if [ ! -f "/db-init/done" ]; then
   echo 'ActiveRecord::Base.connection.execute(IO.read(".docker/database-init.sql"))' | bundle exec rails console
-  echo 'user = Decidim::User.where(email: "decidim-ocl@puzzle.ch").first; user.invite!; "*************************************************** ACTIVATE YOUR ACCOUNT **************************************************** #{user.email} on http://localhost:3000/users/invitation/accept?invitation_token=#{user.raw_invitation_token}"' | bundle exec rails console
+  echo 'user = Decidim::User.where(email: "decidim@puzzle.ch").first; user.invite!; "*************************************************** ACTIVATE YOUR ACCOUNT **************************************************** #{user.email} on http://localhost:3000/users/invitation/accept?invitation_token=#{user.raw_invitation_token}"' | bundle exec rails console
   date > /db-init/done
 else
   echo "Skipping database initialization because already done on $(cat /db-init/done)"
