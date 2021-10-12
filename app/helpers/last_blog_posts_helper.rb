6f65e5fb8cc3fce85a37a5f49de8c1cafa4c76e0
Carlo Beltrame <carlo@beltra.me>
2021-10-12 10:14:05 +0200

    Prefer to use an image embedded in the blog post text

--- app/helpers/last_blog_posts_helper.rb
+++ app/helpers/last_blog_posts_helper.rb
@@ -2,6 +2,13 @@
 
 module LastBlogPostsHelper
   def blog_image_src(post)
+    image_from_text = Nokogiri::HTML
+        .parse(translated_attribute post.body)
+        .css('img')
+        .first
+        &.attr('src')
+
+    return image_from_text if image_from_text.present?
     return post.photos.sort_by(&:weight).first.url if post.photos.count > 0
     'organization-default-image.png'
   end
