cea57ba092495d7d9cccbc15052ba4cf11df6897
Carlo Beltrame <carlo@beltra.me>
2022-02-03 15:33:48 +0100

    Geographic adjectives that end in -er are capitalized in German
    https://www.vogt-text.ch/vogtblog/97-die-geografie-und-ihre-adjektive

--- config/initializers/decidim_questions.rb
+++ config/initializers/decidim_questions.rb
@@ -1,7 +1,7 @@
 Decidim::QuestionCaptcha.configure do |config|
   config.questions = {
       de: [
-          { "question" => "Nenne eine Farbe die im luzerner Wappen vorkommt.", "answers" => "weiss,blau,Weiss,Blau" }
+          { "question" => "Nenne eine Farbe die im Luzerner Wappen vorkommt.", "answers" => "weiss,blau,Weiss,Blau" }
       ]
   }
 end
