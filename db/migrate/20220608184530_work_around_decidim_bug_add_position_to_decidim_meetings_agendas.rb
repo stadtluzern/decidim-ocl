161152a1a999d9e5993a173c51d360d2a2798303
Thomas Burkhalter <new.mysteryman@gmail.com>
2022-06-08 20:50:32 +0200

    Work around decidim bug by adding a column

---
+++ db/migrate/20220608184530_work_around_decidim_bug_add_position_to_decidim_meetings_agendas.rb
@@ -0,0 +1,5 @@
+class WorkAroundDecidimBugAddPositionToDecidimMeetingsAgendas < ActiveRecord::Migration[6.0]
+  def change
+    add_column :decidim_meetings_agendas, :position, :integer, default: 0
+  end
+end
