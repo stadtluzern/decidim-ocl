f3383571561300a046721b486a93cbcab75f2efe
Thomas Burkhalter <new.mysteryman@gmail.com>
2022-06-13 22:51:48 +0200

    Fix gis_luzern js import

--- app/providers/decidim/map/provider/dynamic_map/gis_luzern.rb
+++ app/providers/decidim/map/provider/dynamic_map/gis_luzern.rb
@@ -14,7 +14,7 @@ module Decidim
           class Builder < Decidim::Map::DynamicMap::Builder
             # @see Decidim::Map::DynamicMap::Builder#javascript_snippets
             def javascript_snippets
-              template.javascript_include_tag('decidim/gis_luzern')
+              template.javascript_pack_tag('decidim/gis_luzern')
             end
           end
         end
