31d2ccbc3edbdd2ca3f2289235d91d67b2cc8aef
Carlo Beltrame <carlo@beltra.me>
2022-07-21 17:40:10 +0200

    Fix broken migration after initiative uninstallation

--- db/migrate/20210811125883_add_followable_counter_cache_to_initiatives.decidim_initiatives.rb
+++ db/migrate/20210811125883_add_followable_counter_cache_to_initiatives.decidim_initiatives.rb
@@ -2,13 +2,17 @@
 # This migration comes from decidim_initiatives (originally 20210310120720)
 
 class AddFollowableCounterCacheToInitiatives < ActiveRecord::Migration[5.2]
+  class Initiative < ApplicationRecord
+    self.table_name = :decidim_initiatives
+  end
+
   def change
     add_column :decidim_initiatives, :follows_count, :integer, null: false, default: 0, index: true
 
     reversible do |dir|
       dir.up do
-        Decidim::Initiative.reset_column_information
-        Decidim::Initiative.find_each do |record|
+        Initiative.reset_column_information
+        Initiative.find_each do |record|
           record.class.reset_counters(record.id, :follows)
         end
       end
