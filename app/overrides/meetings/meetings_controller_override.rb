47fbfca12d6fb2061380f8550b8e57521ac7ad3f
Thomas Burkhalter <new.mysteryman@gmail.com>
2023-05-31 16:43:06 +0200

    Show all meetings, sort meetings by desc start time
    Fixes #62

--- app/overrides/meetings/meetings_controller_override.rb
+++ app/overrides/meetings/meetings_controller_override.rb
@@ -2,6 +2,19 @@
 
 Decidim::Meetings::MeetingsController.class_eval do
   def meetings
-    @meetings ||= paginate(search.results.order(:start_time))
+    @meetings ||= paginate(search.results.order(start_time: :desc))
+  end
+
+  def default_filter_params
+    {
+      date: ["past", "upcoming"],
+      search_text: "",
+      activity: "all",
+      scope_id: default_filter_scope_params,
+      space: default_filter_space_params,
+      type: default_filter_type_params,
+      origin: default_filter_origin_params,
+      category_id: default_filter_category_params
+    }
   end
 end
