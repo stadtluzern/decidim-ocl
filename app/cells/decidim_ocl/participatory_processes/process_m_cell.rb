15f20eb8b29bbf5e0510ef6da9c4458696ead818
Thomas Burkhalter <new.mysteryman@gmail.com>
2023-06-29 10:26:12 +0200

    Fix image error in rare cases

--- app/cells/decidim_ocl/participatory_processes/process_m_cell.rb
+++ app/cells/decidim_ocl/participatory_processes/process_m_cell.rb
@@ -9,7 +9,15 @@ module DecidimOCL
 
       included do
         def resource_image_path
-          model.attached_uploader(:hero_image).path || rails_blob_path(current_organization.highlighted_content_banner_image)
+          model.attached_uploader(:hero_image).path || default_image
+        rescue StandardError
+          default_image
+        end
+
+        private
+
+        def default_image
+          rails_blob_path(current_organization.highlighted_content_banner_image)
         end
       end
     end
