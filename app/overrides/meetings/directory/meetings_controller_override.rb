9b1164e191571e97fc5e9f31f6b5130c9a105dee
Thomas Burkhalter <new.mysteryman@gmail.com>
2023-07-20 20:47:38 +0200

    Replace default_filter_params

--- app/overrides/meetings/directory/meetings_controller_override.rb
+++ app/overrides/meetings/directory/meetings_controller_override.rb
@@ -7,15 +7,15 @@ Decidim::Meetings::Directory::MeetingsController.class_eval do
 
   def default_filter_params
     {
-      date: ["past", "upcoming"],
-      title_or_description_cont: "",
       search_text: "",
+      with_any_date: ["past", "upcoming"],
+      title_or_description_cont: "",
       activity: "all",
-      scope_id: default_filter_scope_params,
-      space: default_filter_space_params,
-      type: default_filter_type_params,
-      origin: default_filter_origin_params,
-      category_id: default_filter_category_params
+      with_any_scope: default_filter_scope_params,
+      with_any_space: default_filter_space_params,
+      with_any_type: default_filter_type_params,
+      with_any_origin: default_filter_origin_params,
+      with_any_global_category: default_filter_category_params
     }
   end
 end
