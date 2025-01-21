9f4c2d9ad0d77d4cf133d13d495ea14a270ac5a8
Thomas Burkhalter <new.mysteryman@gmail.com>
2025-01-21 01:08:35 +0100

    Fix reply_to in mails

--- app/mailers/decidim_ocl/application_mailer.rb
+++ app/mailers/decidim_ocl/application_mailer.rb
@@ -5,8 +5,13 @@ module DecidimOCL
   # mailers.
   module ApplicationMailer
     def set_smtp
-      reply_to = I18n.t('decidim_ocl.reply_to', default: '').presence
-      mail.reply_to = [reply_to] if mail.reply_to.blank? && reply_to
+      opts = { scope: 'decidim_ocl.reply_to', default: '' }
+      tenant_type = organization.tenant_type.presence
+
+      reply_to = I18n.t(tenant_type, **opts).presence if tenant_type
+      reply_to ||= I18n.t(:default, **opts).presence
+
+      mail.reply_to = reply_to && [reply_to]
 
       super
     end
