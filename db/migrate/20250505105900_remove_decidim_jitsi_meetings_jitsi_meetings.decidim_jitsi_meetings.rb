1ccb4ebf083d315e497043493706dbb95b27e8b3
Thomas Burkhalter <new.mysteryman@gmail.com>
2025-05-14 16:41:45 +0200

    Require original migration before referencing it

--- db/migrate/20250505105900_remove_decidim_jitsi_meetings_jitsi_meetings.decidim_jitsi_meetings.rb
+++ db/migrate/20250505105900_remove_decidim_jitsi_meetings_jitsi_meetings.decidim_jitsi_meetings.rb
@@ -1,5 +1,7 @@
 # This migration comes from decidim_jitsi_meetings (originally 20200406135458)
 class RemoveDecidimJitsiMeetingsJitsiMeetings < ActiveRecord::Migration[5.2]
+  require_relative '20210121143051_create_decidim_jitsi_meetings_jitsi_meetings.decidim_jitsi_meetings'
+
   def change
     revert CreateDecidimJitsiMeetingsJitsiMeetings
   end
