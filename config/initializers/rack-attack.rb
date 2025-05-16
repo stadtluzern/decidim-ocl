40ccc0c8c55363013ae32025557fe44b1dca01f9
Thomas Burkhalter <new.mysteryman@gmail.com>
2025-05-16 11:17:28 +0200

    Improve rack_attack handling

---
+++ config/initializers/rack-attack.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rack/attack'
+require 'ipaddr'
+require_relative '../../lib/rack_attack_helper'
+
+rack = DecidimOCL::RackAttackHelper
+
+Rack::Attack.enabled = rack.enabled?
+rack.safelist_ips_from_env
+rack.register_s3_redirects
+rack.register_throttle_filter_from_env 'requests by ip', 'RACK_ATTACK_FILTER_BY_IP'
+rack.register_allow2ban_filter_from_env 'secure admin logins', 'RACK_ATTACK_FILTER_ADMIN_LOGIN' do |req|
+  req.post? && req.path.include?('system')
+end
+
+rack.subscribe_to_notifications
+rack.subscribe_to_debug_notifications if rack.debug?
