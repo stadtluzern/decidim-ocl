0cb8054a51cb382beb57cfdea8e67ad27a133a90
Thomas Burkhalter <new.mysteryman@gmail.com>
2025-05-16 11:17:28 +0200

    Improve rack_attack handling

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,19 +1,18 @@
+# frozen_string_literal: true
+
 require 'rack/attack'
 require 'ipaddr'
+require_relative '../../lib/rack_attack_helper'
 
-Rack::Attack.enabled = ENV.fetch('ENABLE_RACK_ATTACK', Rails.env.production?.to_s).in?(%w[true 1])
+rack = DecidimOCL::RackAttackHelper
 
-ActiveSupport::Notifications.subscribe(/rack_attack/) do |name, start, finish, request_id, payload|
-  # request object available in payload[:request]
-  request = payload[:request]
-  Rails.logger.warn "RACK ATTACK #{name} - #{request.ip} - #{request.url}"
+Rack::Attack.enabled = rack.enabled?
+rack.safelist_ips_from_env
+rack.register_s3_redirects
+rack.register_throttle_filter_from_env 'requests by ip', 'RACK_ATTACK_FILTER_BY_IP'
+rack.register_allow2ban_filter_from_env 'secure admin logins', 'RACK_ATTACK_FILTER_ADMIN_LOGIN' do |req|
+  req.post? && req.path.include?('system')
 end
 
-safelist_ips = ENV.fetch('RACK_ATTACK_SAFELIST_IPS', '').split(',').map(&:strip)
-
-safelist_ips.each do |ip_or_subnet|
-  # Fails if the ip/subnet is not correct
-  IPAddr.new(ip_or_subnet)
-
-  Rack::Attack.safelist_ip(ip_or_subnet)
-end
+rack.subscribe_to_notifications
+rack.subscribe_to_debug_notifications if rack.debug?
