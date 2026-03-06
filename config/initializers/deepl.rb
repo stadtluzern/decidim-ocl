5c2fc4ccbd1a92a7718d5933eb8e634ab6aa3770
carlobeltrame <carlo@beltra.me>
2026-03-06 12:25:50 +0100

    Reset cached DeepL configuration after setting secrets
    For some reason, nobody else has had this problem yet, but it is the
    sole cause of "Missing Authorization header" errors.
    Refs https://github.com/decidim-swiss/decidim.swiss/issues/212

--- config/initializers/deepl.rb
+++ config/initializers/deepl.rb
@@ -5,3 +5,7 @@ DeepL.configure do |config|
   config.auth_key = Rails.application.secrets.translator[:secret_key]
   config.host = Rails.application.secrets.translator[:endpoint_url]
 end
+
+# Need to reset cached values inside the deepl client library because we
+# may change the host from api.deepl.com to api-free.deepl.com
+DeepL.instance_variable_set(:@api, nil)
